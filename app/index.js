c91faa88d4b93d10a0b83c8a2a78d01c43c54f03
Felipe Henssler Koetz <felipehenssler@gmail.com>
2023-09-03 16:58:10 -0300

    chore: base node chain

---
+++ app/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const Blockchain = require('../blockchain');
+const bodyParser = require('body-parser');
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const blockchain = new Blockchain();
+const P2pserver = require('./p2p-server');
+const p2pserver = new P2pserver(blockchain);
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  p2pserver.syncChain();
+  res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log(`listening on port ${HTTP_PORT}`);
+})
+
+p2pserver.listen();
